7918c2111c61c70c35d8647931c7d363bfcd0d9f
Eric <erikbc_95@hotmail.com>
2019-08-20 14:59:05 +0200

    added Switch UI utility component

---
+++ Assets/Simple_UI/Simple_Switch/ExampleSwitchController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ExampleSwitchController : MonoBehaviour
+{
+    [SerializeField] private SwitchComponent switchComponent = null;
+    [SerializeField] private SwitchComponent switchComponent_big = null;
+
+    private void Start()
+    {
+        if(switchComponent != null)
+            this.switchComponent.OnValueSwapTo = (isEnable) => { print("SMALL switchComponent state is: "+ isEnable); };
+
+        if(switchComponent_big != null)
+            this.switchComponent_big.OnValueSwapTo = (isEnable) => { print("BIG switchComponent state is: "+isEnable); };
+    }    
+}
---
+++ Assets/Simple_UI/Simple_Switch/SwitchComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchComponent : MonoBehaviour
+{
+    public Action<bool> OnValueSwapTo = null;
+
+    private Slider slider = null;    
+    [SerializeField] private bool isEnable = true;
+    [SerializeField] private Color enableColor = Color.white;
+    [SerializeField] private Color disableColor = Color.black;
+    private float newValue = 0;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        slider.handleRect.gameObject.GetComponent<Button>().onClick.AddListener(OnKnotPressed);
+    }
+
+    private void OnKnotPressed()
+    {
+        newValue = (slider.value == 0) ? 1 : 0;
+        isEnable = (newValue == 0) ? true : false;
+        slider.value = newValue;
+        ChangeStyle();
+
+        OnValueSwapTo?.Invoke(isEnable);//Trigger all actions stacked on that event
+    }
+
+    private void ChangeStyle()
+    {
+        slider.fillRect.gameObject.GetComponent<Image>().color = (isEnable) ? enableColor : disableColor;
+        slider.handleRect.gameObject.GetComponent<Image>().color = (isEnable) ? enableColor : disableColor;
+    }    
+}
