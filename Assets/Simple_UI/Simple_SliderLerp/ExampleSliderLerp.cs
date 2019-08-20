2c081612c29cbc13890327449c7817f35f876259
Eric <erikbc_95@hotmail.com>
2019-08-20 15:34:27 +0200

    added SliderLerp UI Utility

---
+++ Assets/Simple_UI/Simple_SliderLerp/ExampleSliderLerp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleSliderLerp : MonoBehaviour
+{
+    [Header("Slider Simple Example")]
+    [SerializeField] private SliderLerpComponent sliderLerp = null;
+    [SerializeField] private Button buttonMax = null;
+    [SerializeField] private Button buttonMin = null;
+    [Header("Slider Menu Example")]
+    [SerializeField] private SliderLerpComponent sliderLerpMenu = null;
+    [SerializeField] private List<Button> menuButtons = new List<Button>();
+
+    private void Start()
+    {
+        for(int i = 0; i < menuButtons.Count; i++)        
+            menuButtons[i].onClick.AddListener(()=> { sliderLerpMenu.LerpToValue((float)i); });
+
+        buttonMax.onClick.AddListener(() => {sliderLerp.LerpToValue(sliderLerp.GetMaxValue());});
+        buttonMin.onClick.AddListener(() => {sliderLerp.LerpToValue(sliderLerp.GetMinValue());});
+    }
+}
---
+++ Assets/Simple_UI/Simple_SliderLerp/SliderLerpComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderLerpComponent : MonoBehaviour
+{
+    private Slider sliderComp;
+    private float actualValue = 0;
+    private float interpolation = 0.5f;
+
+    private void Start()
+    {
+        sliderComp = GetComponent<Slider>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (actualValue != sliderComp.value)        
+            sliderComp.value = Mathf.Lerp(sliderComp.value, actualValue, interpolation);        
+    }
+
+    public void LerpToValue(float value)
+    {
+        actualValue = value;
+    }
+
+    public float GetMaxValue()
+    {
+        return sliderComp.maxValue;
+    }
+    public float GetMinValue()
+    {
+        return sliderComp.minValue;
+    }
+}
