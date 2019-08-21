f1ff82fcc9b3c864998619bb1c05915d1d177854
Eric <erikbc_95@hotmail.com>
2019-08-21 10:34:10 +0200

    added EmbeddedBrowser Utility for Mobile Apps (only Simple_Example with the new version, but there is the pckg of the old plugin)

---
+++ Assets/Simple_EmbeddedBrowser/Simple_NewPlugin/BrowserOpener.cs
@@ -0,0 +1,56 @@
+﻿public class BrowserOpener : Singleton<BrowserOpener>
+{
+    private string pageToOpen = "https://www.google.com/";
+
+    // check readme file to find out how to change title, colors etc.
+    public void OnButtonClicked(string pageToOpen)
+    {
+        this.pageToOpen = pageToOpen;
+		InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
+		options.displayURLAsPageTitle = false;
+		options.pageTitle = "PAGE TITLE";
+        options.backButtonText = "<";
+        options.backButtonFontSize = "30";
+        options.barBackgroundColor = "#B10034";
+        options.textColor = "#FFFFFF";
+        /*
+        int paddingTop = 100;
+        int paddingBottom = 100;
+        int paddingLeft = 100;
+        int paddingRight = 100;
+        InAppBrowser.EdgeInsets insets = new InAppBrowser.EdgeInsets(paddingTop,
+        paddingBottom, paddingLeft, paddingRight);
+        options.insets = insets;
+        */
+        InAppBrowser.OpenURL(pageToOpen, options);
+        print("Opened browser in: "+pageToOpen);
+	}
+
+    public void OnButtonClicked(string pageToOpen, string pageTitle)
+    {
+        this.pageToOpen = pageToOpen;
+        InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
+        options.displayURLAsPageTitle = false;
+        options.pageTitle = pageTitle;
+        options.backButtonText = "<";
+        options.backButtonFontSize = "30";
+        options.barBackgroundColor = "#B10034";
+        options.textColor = "#FFFFFF";
+        /*
+        int paddingTop = 100;
+        int paddingBottom = 100;
+        int paddingLeft = 100;
+        int paddingRight = 100;
+        InAppBrowser.EdgeInsets insets = new InAppBrowser.EdgeInsets(paddingTop,
+        paddingBottom, paddingLeft, paddingRight);
+        options.insets = insets;
+        */
+        InAppBrowser.OpenURL(pageToOpen, options);
+        print("Opened browser in: " + pageToOpen);
+    }
+
+    public void OnClearCacheClicked()
+    {
+		InAppBrowser.ClearCache();
+	}
+}
---
+++ Assets/Simple_EmbeddedBrowser/Simple_NewPlugin/ExampleEmbeddedBrowserController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleEmbeddedBrowserController : MonoBehaviour
+{
+    [SerializeField] private string pageToOpen = "https://www.google.com/";
+    [Space()]
+    [SerializeField] private Button openerButton = null;
+    [SerializeField] private Text infoText = null;
+
+    private void Awake()
+    {
+        openerButton.onClick.AddListener(OpenBrowser);
+    }
+
+    private void OpenBrowser()
+    {
+        BrowserOpener.s_Instance.OnButtonClicked(pageToOpen);
+        infoText.text = "Opened browser in: " + pageToOpen;
+        infoText.text += "\n (Remember that you can only see it on Mobile Builds!!!)";
+    }
+}
