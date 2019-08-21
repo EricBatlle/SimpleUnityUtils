89d4cc329fb1ecd243b135fcacb80f453e603ccd
Eric <erikbc_95@hotmail.com>
2019-08-21 11:58:30 +0200

    modified Simple_UI LoginRegister to deacoplate UIController code so it will be easier to use on cases like Simple_DB

--- Assets/Simple_UI/Login+Register/Scripts/Login.cs
+++ Assets/Simple_UI/Login+Register/Scripts/Login.cs
@@ -1,36 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Login : MonoBehaviour
+namespace Simple_UI
 {
-    [Header("Login Fields")]
-    [SerializeField] private InputField usernameInput = null;
-    [SerializeField] private InputField passwordInput = null;
-    [Header("Display Information")]
-    [SerializeField] private Text infoText = null;
-    [Header("Buttons")]
-    [SerializeField] private Button confirmButton = null;
-    [SerializeField] private Button registerButton = null;
-    [SerializeField] private Button loginLaterButton = null;
-
-    private void Awake()
+    public class Login : MonoBehaviour
     {
-        confirmButton.onClick.AddListener(() => { UIController.s_Instance.OnConfirmLoginButtonClick(GetUsername(), GetPassword()); });
-        registerButton.onClick.AddListener(UIController.s_Instance.OnGoRegisterButtonClick);
-        loginLaterButton.onClick.AddListener(UIController.s_Instance.OnGoLoginLater);
-    }
+        [Header("Login Fields")]
+        [SerializeField] private InputField usernameInput = null;
+        [SerializeField] private InputField passwordInput = null;
+        [Header("Display Information")]
+        [SerializeField] private Text infoText = null;
+        [Header("Buttons")]
+        [SerializeField] private Button confirmButton = null;
+        [SerializeField] private Button registerButton = null;
+        [SerializeField] private Button loginLaterButton = null;
+
+        private void Awake()
+        {
+            confirmButton.onClick.AddListener(() => { UIController.s_Instance.OnConfirmLoginButtonClick(GetUsername(), GetPassword()); });
+            registerButton.onClick.AddListener(UIController.s_Instance.OnGoRegisterButtonClick);
+            loginLaterButton.onClick.AddListener(UIController.s_Instance.OnGoLoginLater);
+        }
     
-    public string GetUsername()
-    {
-        return usernameInput.text;
-    }
-    private string GetPassword()
-    {
-        return passwordInput.text;
-    }
+        public string GetUsername()
+        {
+            return usernameInput.text;
+        }
+        private string GetPassword()
+        {
+            return passwordInput.text;
+        }
 
-    public void SetInformationPanel(string newInfo)
-    {
-        this.infoText.text = newInfo;
+        public void SetInformationPanel(string newInfo)
+        {
+            this.infoText.text = newInfo;
+        }
     }
 }
--- Assets/Simple_UI/Login+Register/UIController.cs
+++ Assets/Simple_UI/Login+Register/UIController.cs
@@ -1,172 +1,140 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class UIController : Singleton<UIController>
+namespace Simple_UI
 {
-    [Header("UIComponents")]
-    [SerializeField] private Login login = null;    
-    [SerializeField] private Register register = null;    
-    [SerializeField] private MainMenu mainMenu = null;
-    [SerializeField] private Text debugInformationText = null;
-
-    [Header("Users")]
-    [SerializeField] private List<User> registerUsersList = new List<User>();
-    [SerializeField] private User currentUser = null;
-    public User CurrentUser
+    public class UIController : Singleton<UIController>
     {
-        get { return currentUser; }
-        set //Not called if change property from editor
+        [Header("UIComponents")]
+        [SerializeField] private Login login = null;    
+        [SerializeField] private Register register = null;    
+        [SerializeField] private MainMenu mainMenu = null;
+    
+        public void StartUI(Action<string> OnSuccess = null)
         {
-            currentUser = value;
-            SetDebugInformation();
+            //Close everything in case is open
+            CloseMainMenu();
+            CloseRegister();
+            //Starts UI screen with the Login panel
+            OpenLogin();
         }
-    }
 
-    private void Start()
-    {
-        StartUI();
-    }
-
-    public void StartUI(Action<string> OnSuccess = null)
-    {
-        //Close everything in case is open
-        CloseMainMenu();
-        CloseRegister();
-        //Starts UI screen with the Login panel
-        OpenLogin();
-    }
-
-    private void SetDebugInformation()
-    {
-        this.debugInformationText.text =
-            "DEBUG INFORMATION \n";
-
-        if(this.currentUser != null)
+        #region OpenClose UI components
+        #region LOGIN
+        public void OpenLogin()
         {
-            this.debugInformationText.text +=
-            "Username: " + this.currentUser.Username + "\n" +
-            "Password: " + this.currentUser.Password + "\n" +
-            "Email: " + this.currentUser.Email;
+            if(!login.gameObject.activeSelf)
+                login.gameObject.SetActive(true);
+        }    
+        public void CloseLogin()
+        {
+            if (login.gameObject.activeSelf)
+                login.gameObject.SetActive(false);
         }
-    }
-
-    #region OpenClose UI components
-    #region LOGIN
-    public void OpenLogin()
-    {
-        if(!login.gameObject.activeSelf)
-            login.gameObject.SetActive(true);
-    }    
-    public void CloseLogin()
-    {
-        if (login.gameObject.activeSelf)
-            login.gameObject.SetActive(false);
-    }
-    #endregion
-    #region REGISTER
-    public void OpenRegister()
-    {
-        if (!register.gameObject.activeSelf)
-            register.gameObject.SetActive(true);
-    }    
-    public void CloseRegister()
-    {
-        if (register.gameObject.activeSelf)
-            register.gameObject.SetActive(false);
-    }
-    #endregion
-    #region MAINMENU
-    public void OpenMainMenu()
-    {
-        if (!mainMenu.gameObject.activeSelf)
-            mainMenu.gameObject.SetActive(true);
-    }
-    public void CloseMainMenu()
-    {
-        if (mainMenu.gameObject.activeSelf)
-            mainMenu.gameObject.SetActive(false);
-    }
-    #endregion
-    #endregion
-
-    #region OnClickUIEventsNavigation
-    #region LOGIN
-    public void OnGoLoginLater()
-    {
-        CloseLogin();
-        OpenMainMenu();
-    }
-    public void OnConfirmLoginButtonClick(string username, string password)
-    {
-        if(VerifyLogin(username, password))
+        #endregion
+        #region REGISTER
+        public void OpenRegister()
         {
-            CurrentUser = registerUsersList.Single(u => (u.Username == username)&&(u.Password == password));
-            CloseLogin();            
+            if (!register.gameObject.activeSelf)
+                register.gameObject.SetActive(true);
+        }    
+        public void CloseRegister()
+        {
+            if (register.gameObject.activeSelf)
+                register.gameObject.SetActive(false);
+        }
+        #endregion
+        #region MAINMENU
+        public void OpenMainMenu()
+        {
+            if (!mainMenu.gameObject.activeSelf)
+                mainMenu.gameObject.SetActive(true);
+        }
+        public void CloseMainMenu()
+        {
+            if (mainMenu.gameObject.activeSelf)
+                mainMenu.gameObject.SetActive(false);
+        }
+        #endregion
+        #endregion
+
+        #region OnClickUIEventsNavigation
+        #region LOGIN
+        public void OnGoLoginLater()
+        {
+            CloseLogin();
             OpenMainMenu();
         }
-    }
-    public void OnGoRegisterButtonClick()
-    {
-        CloseLogin();
-        OpenRegister();
-    }
-    private bool VerifyLogin(string username, string password)
-    {
-        //Check if the username exists and if it match with their password
-        if (registerUsersList.Any(u => (u.Username == username) && (u.Password == password)))
+        public virtual void OnConfirmLoginButtonClick(string username, string password)
         {
-            UIController.s_Instance.login.SetInformationPanel("");
-            return true;
+            if(VerifyLogin(username, password))
+            {
+                ExampleLoginRegisterController.s_Instance.SetCurrentUser(username,password);            
+                CloseLogin();            
+                OpenMainMenu();
+            }
         }
-        else if(registerUsersList.Any(u => u.Username == username))
-            this.login.SetInformationPanel("Wrong credentials");
-        else
-            this.login.SetInformationPanel("This user does not exist");        
-       
-        return false;
-    }
-    #endregion
-    #region REGISTER
-    public void OnConfirmRegisterButtonClick(User user)
-    {   
-        if(VerifyRegister(user))
+        public void OnGoRegisterButtonClick()
         {
-            registerUsersList.Add(user);
+            CloseLogin();
+            OpenRegister();
+        }
+        protected virtual bool VerifyLogin(string username, string password)
+        {
+            //Check if the username exists and if it match with their password
+            if (ExampleLoginRegisterController.s_Instance.VerifyIfUsernameMatchPassword(username, password))
+            {
+                login.SetInformationPanel("");
+                return true;
+            }
+            else if (ExampleLoginRegisterController.s_Instance.VerifyIfUsernameExists(username))
+                this.login.SetInformationPanel("Wrong credentials");
+            else
+                this.login.SetInformationPanel("This user does not exist");        
+       
+            return false;
+        }
+        #endregion
+        #region REGISTER
+        public virtual void OnConfirmRegisterButtonClick(User user)
+        {   
+            if(VerifyRegister(user))
+            {
+                ExampleLoginRegisterController.s_Instance.RegisterNewUser(user);
+                CloseRegister();
+                OpenLogin();
+            }
+        }
+        public void OnGoBackToLoginFromRegisterButtonClick()
+        {
             CloseRegister();
             OpenLogin();
         }
-    }
-    public void OnGoBackToLoginFromRegisterButtonClick()
-    {
-        CloseRegister();
-        OpenLogin();
-    }
-    private bool VerifyRegister(User user)
-    {
-        //Check if the username or email already exists
-        if(registerUsersList.Any(u => u.Username == user.Username))        
-            this.register.SetInformationPanel("This username is already taken");
-        else if(registerUsersList.Any(u => u.Email == user.Email))        
-            this.register.SetInformationPanel("This email is already taken");
-        else
+        protected virtual bool VerifyRegister(User user)
         {
-            this.login.SetInformationPanel("");
-            return true;
-        }
+            //Check if the username or email already exists
+            if(ExampleLoginRegisterController.s_Instance.VerifyIfUsernameExists(user.Username))        
+                this.register.SetInformationPanel("This username is already taken");
+            else if(ExampleLoginRegisterController.s_Instance.VerifyIfEmailExists(user.Email))        
+                this.register.SetInformationPanel("This email is already taken");
+            else
+            {
+                this.login.SetInformationPanel("");
+                return true;
+            }
         
-        return false;
+            return false;
+        }
+        #endregion
+        #region MAINMENU
+        public void OnGoBackToLoginFromMainMenuButtonClick()
+        {
+            CloseMainMenu();
+            OpenLogin();
+            ExampleLoginRegisterController.s_Instance.SetCurrentUser(null,null);
+            //CurrentUser = null;
+        }
+        #endregion
+        #endregion
     }
-    #endregion
-    #region MAINMENU
-    public void OnGoBackToLoginFromMainMenuButtonClick()
-    {
-        CloseMainMenu();
-        OpenLogin();
-        CurrentUser = null;
-    }
-    #endregion
-    #endregion
 }
