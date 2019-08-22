78b6161281da091e460dfebde945253961f987f3
Eric <erikbc_95@hotmail.com>
2019-08-22 15:16:22 +0200

    splited UIController code into UIController and LoginRegister controller, to make easy the abstraction and implementations of the inheritated classes from LoginRegisterController

--- Assets/Simple_UI/Login+Register/UIController.cs
+++ Assets/Simple_UI/Login+Register/UIController.cs
@@ -3,143 +3,73 @@ using UnityEngine;
 
 namespace Simple_UI
 {
-    public class UIController : Singleton<UIController>
+    public class UIController : LoginRegisterController
     {
-        [Header("UIComponents")]
-        [SerializeField] protected Login login = null;    
-        [SerializeField] protected Register register = null;    
-        [SerializeField] protected MainMenu mainMenu = null;
-    
-        public void StartUI(Action<string> OnSuccess = null)
-        {
-            //Close everything in case is open
-            CloseMainMenu();
-            CloseRegister();
-            //Starts UI screen with the Login panel
-            OpenLogin();
-        }
-
-        #region OpenClose UI components
         #region LOGIN
-        public void OpenLogin()
+        public override void OnConfirmLoginButtonClickImpl(string username, string password)
         {
-            if(!login.gameObject.activeSelf)
-                login.gameObject.SetActive(true);
-        }    
-        public void CloseLogin()
-        {
-            if (login.gameObject.activeSelf)
-                login.gameObject.SetActive(false);
-        }
-        #endregion
-        #region REGISTER
-        public void OpenRegister()
-        {
-            if (!register.gameObject.activeSelf)
-                register.gameObject.SetActive(true);
-        }    
-        public void CloseRegister()
-        {
-            if (register.gameObject.activeSelf)
-                register.gameObject.SetActive(false);
-        }
-        #endregion
-        #region MAINMENU
-        public void OpenMainMenu()
-        {
-            if (!mainMenu.gameObject.activeSelf)
-                mainMenu.gameObject.SetActive(true);
-        }
-        public void CloseMainMenu()
-        {
-            if (mainMenu.gameObject.activeSelf)
-                mainMenu.gameObject.SetActive(false);
-        }
-        #endregion
-        #endregion
-
-        #region OnClickUIEventsNavigation
-        #region LOGIN
-        public void OnGoLoginLater()
-        {
-            CloseLogin();
-            OpenMainMenu();
-        }
-        public virtual void OnConfirmLoginButtonClick(string username, string password)
-        {
-            if(VerifyLogin(username, password))
+            if (VerifyLoginImpl(username, password))
             {
-                ExampleLoginRegisterController.s_Instance.SetCurrentUser(username,password);            
-                CloseLogin();            
+                ExampleLoginRegisterController.s_Instance.SetCurrentUser(username, password);
+                CloseLogin();
                 OpenMainMenu();
             }
         }
-        public void OnGoRegisterButtonClick()
-        {
-            CloseLogin();
-            OpenRegister();
-        }
-        protected virtual bool VerifyLogin(string username, string password)
+
+        protected override bool VerifyLoginImpl(string username, string password)
         {
             //Check if the username exists and if it match with their password
             if (ExampleLoginRegisterController.s_Instance.VerifyIfUsernameMatchPassword(username, password))
             {
-                login.SetInformationPanel("");
+                base.login.SetInformationPanel("");
                 return true;
             }
             else if (ExampleLoginRegisterController.s_Instance.VerifyIfUsernameExists(username))
-                this.login.SetInformationPanel("Wrong credentials");
+                base.login.SetInformationPanel("Wrong credentials");
             else
-                this.login.SetInformationPanel("This user does not exist");        
-       
+                base.login.SetInformationPanel("This user does not exist");
+
             return false;
         }
         #endregion
+
         #region REGISTER
-        public virtual void OnConfirmRegisterButtonClick(User user)
-        {   
-            if(VerifyRegister(user))
+        public override void OnConfirmRegisterButtonClickImpl(object user)
+        {
+            if (VerifyRegisterImpl(user))
             {
-                ExampleLoginRegisterController.s_Instance.RegisterNewUser(user);
+                ExampleLoginRegisterController.s_Instance.RegisterNewUser((User)user);
                 CloseRegister();
                 OpenLogin();
             }
         }
-        public void OnGoBackToLoginFromRegisterButtonClick()
+
+        protected override bool VerifyRegisterImpl(object user)
         {
-            CloseRegister();
-            OpenLogin();
-        }
-        protected virtual bool VerifyRegister(User user)
-        {
+            User userCasted = (User)user;
             //Check if the username or email already exists
-            if(ExampleLoginRegisterController.s_Instance.VerifyIfUsernameExists(user.Username))        
-                this.register.SetInformationPanel("This username is already taken");
-            else if(ExampleLoginRegisterController.s_Instance.VerifyIfEmailExists(user.Email))        
-                this.register.SetInformationPanel("This email is already taken");
+            if (ExampleLoginRegisterController.s_Instance.VerifyIfUsernameExists(userCasted.Username))
+                base.register.SetInformationPanel("This username is already taken");
+            else if (ExampleLoginRegisterController.s_Instance.VerifyIfEmailExists(userCasted.Email))
+                base.register.SetInformationPanel("This email is already taken");
             else
             {
-                this.login.SetInformationPanel("");
+                base.login.SetInformationPanel("");
                 return true;
             }
-        
-            return false;
-        }
-        //Overload for DB porpouse
-        protected virtual bool VerifyRegister(string result)
-        {
+
             return false;
         }
         #endregion
+
         #region MAINMENU
-        public virtual void OnGoBackToLoginFromMainMenuButtonClick()
+        public override void OnGoBackToLoginFromMainMenuButtonClickImpl()
         {
             CloseMainMenu();
             OpenLogin();
-            ExampleLoginRegisterController.s_Instance.SetCurrentUser(null,null);
+            ExampleLoginRegisterController.s_Instance.SetCurrentUser(null, null);
             //CurrentUser = null;
         }
-        #endregion
         #endregion
     }
 }
