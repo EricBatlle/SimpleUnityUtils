1117f5b9fc45f247b0184b1802002dfa21ae81db
Eric <erikbc_95@hotmail.com>
2019-08-23 12:18:22 +0200

    added Simple_DB

---
+++ Assets/Simple_DB/Editor/DBControllerEditor.cs
@@ -0,0 +1,36 @@
+﻿using Simple_DB;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DBController))]
+public class DBControllerEditor : Editor
+{
+    [Header("Post New User")]
+    [SerializeField] public string username = "defaultUserName";
+    [SerializeField] public string password = "defaultPassword";
+    [SerializeField] public string email = "default@email.com";
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        DBController dbController = (DBController)target;
+
+        #region Database functionalities 
+        //Post New User
+        username = EditorGUILayout.TextField("Username:", username);
+        password = EditorGUILayout.TextField("Password:", password);
+        email = EditorGUILayout.TextField("Email:", email);
+
+        if (GUILayout.Button("PostUser"))
+            dbController.PostNewUser((result) => { Debug.Log("Posted New User: " + result); }, new User(username, password, email));
+        GUILayout.Space(20f);
+
+        if (GUILayout.Button("Clean DB"))
+            dbController.CleanDB();
+        GUILayout.Space(20f);        
+        #endregion
+    }
+
+}
---
+++ Assets/Simple_DB/Scripts/UIController.cs
@@ -0,0 +1,89 @@
+﻿namespace Simple_DB
+{
+    /// <summary>
+    /// This class extends the original UIController to modify verification steps...
+    /// ...so it works with DB verifications
+    /// 
+    /// !!!!!WARNING!!!!
+    /// THIS SCRIPT REQUIRES THE Simple_UI/Simple_Login+Register scripts!
+    /// </summary>
+    public class UIController : Simple_UI.LoginRegisterController
+    {
+        #region LOGIN
+        public override void OnConfirmLoginButtonClickImpl(params string[] loginParams)
+        {
+            string username = loginParams[0];
+            string password = loginParams[1];
+            DBController.s_Instance.PostLoginVerify((result) =>
+            {
+            if (VerifyLoginImpl(result))
+            {
+                    string newUserDBString = WebResponse.GetResponseInfo(result);
+                    UserDB newUserDB = JsonManager.DeserializeFromJson<UserDB>(newUserDBString);
+                    ExampleDBController.s_Instance.SetCurrentUser(newUserDB);
+                    CloseLogin();
+                    OpenMainMenu();
+                }
+            }, username, password);
+
+        }
+
+        protected override bool VerifyLoginImpl(params string[] loginParams)
+        {
+            string result = loginParams[0];
+            if (WebResponse.isResultOk(result))
+            {
+                base.login.SetInformationPanel("");
+                return true;
+            }
+            else if (WebResponse.isEqualTo(WebResponse.ERROR_LOGIN_UNEXISTANT_USERNAME, result))
+                base.login.SetInformationPanel("This user does not exist");
+            else if (WebResponse.isEqualTo(WebResponse.ERROR_LOGIN_WRONG_CREDENTIALS, result))
+                base.login.SetInformationPanel("Wrong credentials");
+
+            return false;
+        }        
+        #endregion
+
+        #region REGISTER        
+        public override void OnConfirmRegisterButtonClickImpl(object user)
+        {            
+            DBController.s_Instance.PostNewUser((result) =>
+            {
+                if (VerifyRegisterImpl(result))
+                {
+                    string newUserDBJson = WebResponse.GetResponseInfo(result);
+                    UserDB newUserDB = JsonManager.DeserializeFromJson<UserDB>(newUserDBJson);                    
+                    CloseRegister();
+                    OpenLogin();
+                }
+            }, (User)user);                     
+        }
+
+        protected override bool VerifyRegisterImpl(object resultObject)
+        {
+            string result = (string)resultObject;
+            if (WebResponse.isResultOk(result))
+            {
+                base.login.SetInformationPanel("");
+                return true;
+            }
+            else if (WebResponse.isEqualTo(WebResponse.ERROR_REGISTER_DUPLICATE_USERNAME, result))
+                base.register.SetInformationPanel("This username is already taken");
+            else
+                base.register.SetInformationPanel("Error on register");
+
+            return false;
+        }
+        #endregion
+
+        #region MAINMENU
+        public override void OnGoBackToLoginFromMainMenuButtonClickImpl()
+        {
+            CloseMainMenu();
+            ExampleDBController.s_Instance.SetCurrentUser(null);
+            OpenLogin();
+        }
+        #endregion
+    }
+}
