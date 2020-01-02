8c87e5f72f90c3737365945b3506ee9781a2cece
Eric <erikbc_95@hotmail.com>
2020-01-02 18:11:44 +0100

    added simple animations

---
+++ Assets/Simple_Animations/BreathEffect.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BreathEffect : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tmp = null;
+    [SerializeField] private float effectSpeed = 0.02f;
+    [SerializeField] private float delayEffect = 0.02f;
+
+    private IEnumerator breathCoroutine = null;
+    private float glowPower = 0;
+    private bool isBreathing = true;
+    private static float GLOW_MAX = 1;
+    private static float GLOW_MIN = 0;
+    
+    private void OnEnable()
+    {
+        breathCoroutine = EffectLoop();
+        StartEffect();
+    }
+
+    private void OnDisable()
+    {
+        StopEffect();
+        breathCoroutine = null;
+    }
+
+    public void StartEffect()
+    {
+        StartCoroutine(breathCoroutine);
+    }
+    public void StopEffect()
+    {
+        StopCoroutine(breathCoroutine);
+    }
+    
+    private IEnumerator EffectLoop()
+    {
+        while(true)
+        {
+            glowPower += (isBreathing) ? effectSpeed : -effectSpeed;
+
+            if (isBreathing)            
+                //Increment
+                isBreathing = (glowPower >= GLOW_MAX) ? false : true;            
+            else if (!isBreathing)            
+                //Decrement
+                isBreathing = (glowPower <= GLOW_MIN) ? true : false;
+            
+            //Assign glow power
+            tmp.fontSharedMaterial.SetFloat(ShaderUtilities.ID_GlowPower, glowPower);
+            yield return new WaitForSeconds(delayEffect);
+        }
+    }
+}
---
+++ Assets/Simple_Animations/TextureOffsetLoop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureOffsetLoop : MonoBehaviour
+{
+    public bool GoLoop
+    {
+        get { return goLoop; }
+        set
+        {
+            goLoop = value;
+            if (value == true)
+                StartCoroutine(LoopCoroutine);
+            else if (value == false)
+                StopCoroutine(LoopCoroutine);
+        }
+    }
+    [SerializeField] private bool goLoop = false;
+    [SerializeField] private float offsetIncrement = 5f;
+    private Material material;
+    private IEnumerator LoopCoroutine = null;
+
+    private void Awake()
+    {
+        material = this.GetComponent<MeshRenderer>().material;
+        LoopCoroutine = OffsetCoroutine();
+        StartCoroutine(LoopCoroutine);
+    }    
+
+    private void IncrementOffsetY()
+    {
+        material.mainTextureOffset = new Vector2(material.mainTextureOffset.x, material.mainTextureOffset.y + offsetIncrement);
+    }
+
+    private IEnumerator OffsetCoroutine()
+    {
+        while(goLoop)
+        {
+            IncrementOffsetY();
+            yield return null;
+        }
+    }
+}
