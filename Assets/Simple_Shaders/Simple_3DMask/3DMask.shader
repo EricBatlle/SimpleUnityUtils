a3bb73417f19cf5abda93ec9e447cf7427680bf5
Eric <erikbc_95@hotmail.com>
2020-06-24 15:32:40 +0200

    added SimpleShader to mask 3D objects with 3D masks

---
+++ Assets/Simple_Shaders/Simple_3DMask/3DMask.shader
@@ -0,0 +1,15 @@
+﻿Shader "Masked/3DMask" {
+
+	SubShader{
+		// Render the mask after regular geometry, but before masked geometry and
+		// transparent things.
+		Tags {"Queue" = "Transparent+10" }
+
+		// Don't draw in the RGBA channels; just the depth buffer
+		ColorMask 0
+		ZWrite On
+
+		// Do nothing specific in the pass:
+		Pass {}
+	}
+}
---
+++ Assets/Simple_Shaders/Simple_3DMask/MaskeableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Sets the RenderQueue of an object's materials on Awake. This will instance 
+/// the materials, so the script won't interfere with other renderers that
+/// reference the same materials.
+/// </summary>
+[AddComponentMenu("Rendering/SetRenderQueue")]
+[ExecuteInEditMode]
+public class MaskeableObject : MonoBehaviour
+{
+    [SerializeField]
+    protected int[] m_queues = new int[] { 4020 };
+
+    protected void Awake()
+    {
+        Material[] materials = GetComponent<Renderer>().sharedMaterials;
+        for (int i = 0; i < materials.Length && i < m_queues.Length; ++i)
+        {
+            materials[i].renderQueue = m_queues[i];
+        }
+    }
+}
