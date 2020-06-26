1c172253da323e2556cc301bb915e4480f12a328
Eric <erikbc_95@hotmail.com>
2020-06-26 13:13:31 +0200

    added Simple_shader, ClippingPlane

---
+++ Assets/Simple_Shaders/Simple_ClippingPlane/ClippingPlane.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+public class ClippingPlane : MonoBehaviour
+{
+    [Header("Clip unique object")]
+    public Renderer uniqueRenderer = null;
+    [Header("Clip all material-shared objects")]
+    [Tooltip("If it's not affecting to previous unique object, try to play and unplay scene")]
+    public Material material = null;
+
+    private Plane plane;
+    private MaterialPropertyBlock propBlock = null;
+
+    private void Awake()
+    {
+         /* 
+         * this will be used only if uniqueRenderer != null        
+         * but as is decleared on awake and uniqueRenderer could be modified on the inspector
+         * it's called anyway
+         */
+        propBlock = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        SetClippingPlane();
+    }
+
+    private void SetClippingPlane()
+    {
+        //create plane
+        Plane plane = new Plane(transform.up, transform.position);
+        //transfer values from plane to vector4
+        Vector4 planeRepresentation = new Vector4(plane.normal.x, plane.normal.y, plane.normal.z, plane.distance);
+
+        if (uniqueRenderer != null)
+        {
+            uniqueRenderer.GetPropertyBlock(propBlock);
+            propBlock.SetVector("_Plane", planeRepresentation);
+            uniqueRenderer.SetPropertyBlock(propBlock);
+        }
+        else if (material != null)
+        {
+            material.SetVector("_Plane", planeRepresentation);
+        }
+    }
+
+    #region DRAW GIZMO-PLANE
+    private void OnDrawGizmos()
+    {
+        plane = new Plane(transform.up, transform.position);
+        Vector3 position = transform.position;
+        Vector3 normal = plane.normal;
+        DrawPlane(position,normal);
+    }
+
+    void DrawPlane(Vector3 position, Vector3 normal)
+    {
+        Vector3 v3;
+        if (normal.normalized != Vector3.forward)
+            v3 = Vector3.Cross(normal, Vector3.forward).normalized * normal.magnitude;
+        else
+            v3 = Vector3.Cross(normal, Vector3.up).normalized * normal.magnitude; ;
+
+        Vector3 corner0 = position + v3;
+        Vector3 corner2 = position - v3;
+        Quaternion q = Quaternion.AngleAxis(90.0f, normal);
+        v3 = q * v3;
+        Vector3 corner1 = position + v3;
+        Vector3 corner3 = position - v3;
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(corner0, corner2);
+        Gizmos.DrawLine(corner1, corner3);
+        Gizmos.DrawLine(corner0, corner1);
+        Gizmos.DrawLine(corner1, corner2);
+        Gizmos.DrawLine(corner2, corner3);
+        Gizmos.DrawLine(corner3, corner0);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(position, normal);
+    }
+    #endregion    
+}
---
+++ Assets/Simple_Shaders/Simple_ClippingPlane/ClippingPlane.shader
@@ -0,0 +1,38 @@
+﻿Shader "Masks/ClippingPlane" 
+{
+	Properties
+	{
+	  _MainTex("Texture", 2D) = "white" {}
+	  _BumpMap("Bumpmap", 2D) = "bump" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		Cull Off
+		CGPROGRAM
+		#pragma surface surf Lambert
+		struct Input 
+		{
+			float2 uv_MainTex;
+			float2 uv_BumpMap;
+			float3 worldPos;
+		};
+		sampler2D _MainTex;
+		sampler2D _BumpMap;
+		float4 _Plane;
+
+		void surf(Input IN, inout SurfaceOutput o) 
+		{
+			//Get world position 
+			//_MyVector = mul(unity_WorldToObject, IN.worldPos);
+			float distance = dot(IN.worldPos, _Plane.xyz);
+			distance = distance + _Plane.w;
+			//if world position.y of the object-pixel is > 0, will render
+			clip(-distance);
+			o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb;
+			o.Normal = UnpackNormal(tex2D(_BumpMap, IN.uv_BumpMap));
+		}
+		ENDCG
+	}
+	Fallback "Diffuse"
+}
