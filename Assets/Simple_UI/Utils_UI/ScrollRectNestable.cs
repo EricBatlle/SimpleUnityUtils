3b13c4c6090c4ed6c0d247b0010386b9605c5df4
Eric <erikbc_95@hotmail.com>
2020-08-03 01:32:34 +0200

    added ScrollRectNestable

---
+++ Assets/Simple_UI/Utils_UI/ScrollRectNestable.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class ScrollRectNestable : ScrollRect
+{
+    private bool routeToParent = false;
+
+    /// <summary>
+    /// Do action for all parents
+    /// </summary>
+    private void DoForParents<T>(Action<T> action) where T : IEventSystemHandler
+    {
+        Transform parent = transform.parent;
+        while (parent != null)
+        {
+            foreach (var component in parent.GetComponents<Component>())
+            {
+                if (component is T)
+                    action((T)(IEventSystemHandler)component);
+            }
+            parent = parent.parent;
+        }
+    }
+
+    /// <summary>
+    /// Always route initialize potential drag event to parents
+    /// </summary>
+    public override void OnInitializePotentialDrag(PointerEventData eventData)
+    {
+        DoForParents<IInitializePotentialDragHandler>((parent) => { parent.OnInitializePotentialDrag(eventData); });
+        base.OnInitializePotentialDrag(eventData);
+    }
+    
+    public override void OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        if (routeToParent)
+            DoForParents<IDragHandler>((parent) => { parent.OnDrag(eventData); });
+        else
+            base.OnDrag(eventData);
+    }
+
+    public override void OnBeginDrag(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        if (!horizontal && Math.Abs(eventData.delta.x) > Math.Abs(eventData.delta.y))
+            routeToParent = true;
+        else if (!vertical && Math.Abs(eventData.delta.x) < Math.Abs(eventData.delta.y))
+            routeToParent = true;
+        else
+            routeToParent = false;
+
+        if (routeToParent)
+            DoForParents<IBeginDragHandler>((parent) => { parent.OnBeginDrag(eventData); });
+        else
+            base.OnBeginDrag(eventData);
+    }
+
+    public override void OnEndDrag(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        if (routeToParent)
+            DoForParents<IEndDragHandler>((parent) => { parent.OnEndDrag(eventData); });
+        else
+            base.OnEndDrag(eventData);
+        routeToParent = false;
+    }
+}
