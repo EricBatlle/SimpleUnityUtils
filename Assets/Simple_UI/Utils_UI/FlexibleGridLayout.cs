872a07203e838e8e8df15f0c8e08ee3dc1bccb60
Eric <erikbc_95@hotmail.com>
2020-08-29 14:33:16 +0200

    fixed FlexibleGridLayout Y spacing troubles

--- Assets/Simple_UI/Utils_UI/FlexibleGridLayout.cs
+++ Assets/Simple_UI/Utils_UI/FlexibleGridLayout.cs
@@ -49,8 +49,8 @@ public class FlexibleGridLayout : LayoutGroup
         float parentWidth = rectTransform.rect.width;
         float parentHeight = rectTransform.rect.height;
 
-        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)columns) * (columns - 1)) - (padding.left / (float)columns) - (padding.right / (float)columns);
-        float cellHeight = parentHeight / (float)rows - ((spacing.y / (float)rows) * (columns - 1)) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+        float cellWidth = (parentWidth / (float)columns) - (spacing.x / (float)columns * (columns - 1)) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - (spacing.y / (float)rows * (rows - 1)) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
 
         cellSize.x = fitX ? cellWidth : cellSize.x;
         cellSize.y = fitY ? cellHeight : cellSize.y;
@@ -66,7 +66,7 @@ public class FlexibleGridLayout : LayoutGroup
             var item = rectChildren[i];
 
             var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
-            var yPos = (cellSize.y * rowCount) + (spacing.y * columnCount) + padding.top;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
 
             SetChildAlongAxis(item, 0, xPos, cellSize.x);
             SetChildAlongAxis(item, 1, yPos, cellSize.y);
