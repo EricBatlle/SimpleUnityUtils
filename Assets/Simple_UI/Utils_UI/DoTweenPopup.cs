7f82c59343e580132e28fab9ca6e6fee932995f0
Eric <erikbc_95@hotmail.com>
2021-06-17 06:34:38 +0200

    Update DoTweenPopup

--- Assets/Simple_UI/Utils_UI/DoTweenPopup.cs
+++ Assets/Simple_UI/Utils_UI/DoTweenPopup.cs
@@ -1,47 +1,43 @@
-﻿//using DG.Tweening;
-//using UnityEngine;
+﻿//using UnityEngine;
 
 //public abstract class DoTweenPopup : MonoBehaviour
 //{
-//    [Header("DoTweenPopup")]
-//    [SerializeField] private GameObject panelGO = null;
-//    [Space()]
-//    [SerializeField] protected float duration = 0.35f;
-//    [SerializeField] protected Ease easeIn = Ease.Flash;
-//    [SerializeField] protected Ease easeOut = Ease.Flash;
+//	[Header("DoTweenPopup")]
+//	[SerializeField] protected RectTransform panelRectTransform = null;
+//	[Space()]
+//	[SerializeField] protected float duration = 0.35f;
+//	[SerializeField] protected Ease easeIn = Ease.OutBack;
+//	[SerializeField] protected Ease easeOut = Ease.InBack;
 
-//    protected RectTransform panelRectTransform = null;
-//    protected Tween popupTween = null;
+//	protected Tween popupTween = null;
 
-//    protected virtual void Awake()
-//    {
-//        panelRectTransform = this.panelGO.GetComponent<RectTransform>();
-//        panelRectTransform.localScale = new Vector3(0, 0, 0);
+//	protected virtual void Awake()
+//	{
+//		panelRectTransform.localScale = new Vector3(0, 0, 0);
+//		HidePopup();
+//	}
 
-//        HidePopup();
-//    }
+//	public virtual void ShowPopup()
+//	{
+//		popupTween?.Kill();
+//		popupTween = panelRectTransform.DOScale(new Vector3(1, 1, 1), duration)
+//			.OnStart(OnShowPanelStart)
+//			.OnComplete(OnShowPanelComplete)
+//			.SetEase(easeIn);
+//	}
 
-//    public virtual void ShowPopup()
-//    {
-//        popupTween.Kill();
-//        popupTween = panelRectTransform.DOScale(new Vector3(1, 1, 1), duration)
-//            .OnStart(OnShowPanelStart)
-//            .OnComplete(OnShowPanelComplete)
-//            .SetEase(easeIn);
-//    }
+//	public virtual void HidePopup()
+//	{
+//		popupTween?.Kill();
+//		popupTween = panelRectTransform.DOScale(new Vector3(0, 0, 0), duration)
+//			.OnStart(OnHidePanelStart)
+//			.OnComplete(OnHidePanelComplete)
+//			.SetEase(easeOut);
+//	}
 
-//    public virtual void HidePopup()
-//    {
-//        popupTween.Kill();
-//        popupTween = panelRectTransform.DOScale(new Vector3(0, 0, 0), duration)
-//            .OnStart(OnHidePanelStart)
-//            .OnComplete(OnHidePanelComplete)
-//            .SetEase(easeOut);
-//    }
+//	public virtual void OnShowPanelStart() { }
+//	public virtual void OnShowPanelComplete() { }
 
-//    public virtual void OnShowPanelStart() { }
-//    public virtual void OnShowPanelComplete() { }
-
-//    public virtual void OnHidePanelStart() { }
-//    public virtual void OnHidePanelComplete() { }
+//	public virtual void OnHidePanelStart() { }
+//	public virtual void OnHidePanelComplete() { }
 //}
