2d4be74d635edae14308c165a9ab740620aadf02
Eric <erikbc_95@hotmail.com>
2021-10-19 19:40:27 +0200

    Added MaterialBlockChanger utility

---
+++ Assets/Utils_Global/MaterialBlockChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MaterialBlockChanger : MonoBehaviour
+{
+	private MaterialPropertyBlock block;
+	private Renderer goRenderer = null;
+
+	private void Awake()
+	{
+		block = new MaterialPropertyBlock();
+		goRenderer = this.GetComponent<Renderer>();
+		goRenderer.GetPropertyBlock(block);
+	}
+
+	public void SetTexture(Texture newTexture)
+	{
+		goRenderer.GetPropertyBlock(block);
+		block.SetTexture("_MainTex", newTexture);
+		goRenderer.SetPropertyBlock(block);
+	}
+}
